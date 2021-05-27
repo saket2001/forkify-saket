534e3277ac53703675f0aaaeb8d6e773c81afe7b
saket2001 <saketchandorkar123@gmail.com>
2021-05-27 14:45:47 +0530

    bug testing

--- src/js/controller.js
+++ src/js/controller.js
@@ -60,5 +60,6 @@ const init = function () {
   RecipeView.addHandlerRender(controlRecipes);
   SearchView.addHandlerRender(controlSearchRecipes);
   paginationView.addHandlerClick(controlPagination);
+  console.log(BUG);
 };
 init();
